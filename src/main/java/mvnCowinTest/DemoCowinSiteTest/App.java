45191d066313349340b160d648cc06bbbe0d7ab1
dadebash <das.debashis7779@gmail.com>
2021-05-23 22:22:06 +0530

    first commit

---
+++ src/main/java/mvnCowinTest/DemoCowinSiteTest/App.java
@@ -0,0 +1,13 @@
+package mvnCowinTest.DemoCowinSiteTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mvnCowinTest/DemoCowinSiteTest/AppTest.java
@@ -0,0 +1,20 @@
+package mvnCowinTest.DemoCowinSiteTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void cowinTestMethod()
+    {
+       System.out.println("+++++++++++++++++++++++ this is for testing ++++++++++++++++++++++");
+    }
+}
